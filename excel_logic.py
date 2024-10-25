4b90d4b75f3bf99ae62e92ff77697cb209bee550
Alisa-it <askerrr2002@gmail.com>
2024-10-25 19:07:38 +0300

    added all functional

--- excel_logic.py
+++ excel_logic.py
@@ -1,12 +1,23 @@
 from openpyxl import load_workbook
 
-wb = load_workbook('fam.xlsx')
-sheet = wb["Дано"]
-
 def from_variant(variant):
+    wb = load_workbook('fam.xlsx')
+    sheet = wb["Дано"]
     lst = []
     row_index = variant + 1
-    for cellObj in sheet[f'B{row_index}':f'O{row_index}']:
+    for cellObj in sheet[f'A{row_index}':f'N{row_index}']:
         for cell in cellObj:
             lst.append(cell.value)
+    wb.save("fam.xlsx")
     return lst
+    
+
+def to_variant(lst):
+    wb = load_workbook('fam.xlsx')
+    sheet = wb["Дано"]
+    row_count = sheet.max_row + 1
+
+    for i in range(len(lst)):
+        sheet.cell(row = row_count, column = i+1).value = lst[i]
+    
+    wb.save("fam.xlsx")
